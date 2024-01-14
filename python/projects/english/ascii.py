3838943c966df7805fc5b44e6c7d6265feaab8d0
Menahem_Gertner <meni1547@gmail.com>
2024-01-14 20:37:59 +0200

    add files

--- python/projects/english/ascii.py
+++ python/projects/english/ascii.py
@@ -38,3 +38,32 @@ remote = '''           ⇓ After learning the meaning of the word ⇓
 | Read the last words in English and its interpretation in Hebrew |
 |    and vice versa, read in Hebrew and translate to English!     |
  -----------------------------------------------------------------'''
+
+
+# def multiplication(a, b):
+#     c = 0
+#     for i in range(b):
+#         c += a
+#     return c
+#
+#
+# d = int(input("Enter a number: "))
+# counter = 0
+# res = 0
+# while res < d:
+#     counter += 1
+#     res = multiplication(counter, counter)
+# print(counter)
+
+# def slise(num):
+#     if 1000 > num >= 100 and num % 10 != (num // 10) % 10 and num % 10 != (num // 100) % 10 and (num // 10) % 10 != (num // 100) % 10:
+#         return True
+#
+#
+# user = 0
+# counter = 0
+# while user != 666:
+#     user = int(input("Enter a number: "))
+#     if slise(user):
+#         counter += 1
+# print(counter)
---
+++ מבני נתונים/‏test.py
@@ -0,0 +1,51 @@
+class IntNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __init__(self, value, next):
+        self.value = value
+        self.next = next
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next
+
+    def set_value(self, value):
+        self.value = value
+
+    def set_next(self, next):
+        self.next = next
+
+    def value_to_string(self):
+        return self.value
+
+
+# exercise 5
+num = 2
+first = None
+for i in range(4, 0, -1):
+    first = IntNode(i, first)
+temp = first
+counter = 0
+while temp != None:
+    if num == temp.get_value():
+        counter += 1
+    temp = temp.get_next()
+print(counter)
+
+# exercise 6
+first = None
+for i in range(4, 0, -1):
+    first = IntNode(i, first)
+temp = first
+counter = 0
+total = 0
+while temp != None:
+    counter += 1
+    total += temp.get_value()
+    temp = temp.get_next()
+average = total / counter
+print(average)
